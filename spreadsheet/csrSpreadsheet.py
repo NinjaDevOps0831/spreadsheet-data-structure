6f7253f595e9c644c37658f8b3ee6b6d6c722d7b
Daniel Landaeta <128817762+Daniel-Landaeta@users.noreply.github.com>
2023-04-17 19:03:58 -0700

    csr_based added and project complete...

--- spreadsheet/csrSpreadsheet.py
+++ spreadsheet/csrSpreadsheet.py
@@ -1,5 +1,8 @@
 from spreadsheet.baseSpreadsheet import BaseSpreadsheet
 from spreadsheet.cell import Cell
+import numpy as np
+from scipy.sparse import csr_matrix, find
+
 
 # ------------------------------------------------------------------------
 # This class is required TO BE IMPLEMENTED
@@ -15,7 +18,10 @@ from spreadsheet.cell import Cell
 class CSRSpreadsheet(BaseSpreadsheet):
 
     def __init__(self):
-        # TO BE IMPLEMENTED
+        
+        self.c_row = 0
+        self.c_col = 0
+        
         pass
 
 
@@ -26,6 +32,26 @@ class CSRSpreadsheet(BaseSpreadsheet):
         """
 
         # TO BE IMPLEMENTED
+        cnt = 0
+        max_row = 0
+        max_col = 0
+        datacount = len(lCells)
+        rcv = np.zeros(shape = (3, datacount))
+        for lCell in lCells:            
+            rcv[0,cnt] = lCell.row
+            rcv[1,cnt] = lCell.col
+            rcv[2,cnt] = lCell.val
+            cnt = cnt+1
+            if(max_row < lCell.row):
+                max_row = lCell.row
+            if(max_col < lCell.col):
+                max_col = lCell.col
+
+        self.matrix = csr_matrix((rcv[2], (rcv[0], rcv[1])), shape=(max_row+1, max_col+1))
+       
+        self.c_row = max_row+1
+        self.c_col = max_col+1
+        
         pass
 
 
@@ -37,6 +63,22 @@ class CSRSpreadsheet(BaseSpreadsheet):
         """
 
         # TO BE IMPLEMENTED
+        rcv = find(self.matrix)
+
+        newrcv = np.zeros(shape = (3, len(rcv[2]) + 1))
+        
+        for i in range(len(rcv[2])):
+            newrcv[0,i] = rcv[0][i]
+            newrcv[1,i] = rcv[1][i]
+            newrcv[2,i] = rcv[2][i]
+        newrcv[0,len(rcv[2])] = self.c_row
+        newrcv[1,len(rcv[2])] = self.c_col-1
+        newrcv[2,len(rcv[2])] = 0
+        self.matrix = csr_matrix((newrcv[2], (newrcv[0], newrcv[1])), shape=(self.c_row+1, self.c_col))
+        
+        self.c_row  = self.c_row + 1
+        return True          
+
         pass
 
 
@@ -46,8 +88,20 @@ class CSRSpreadsheet(BaseSpreadsheet):
 
         @return True if operation was successful, or False if not.
         """
-
-        # TO BE IMPLEMENTED
+        rcv = find(self.matrix)
+        newrcv = np.zeros(shape = (3, len(rcv[2]) + 1))
+        
+        for i in range(len(rcv[2])):
+            newrcv[0,i] = rcv[0][i]
+            newrcv[1,i] = rcv[1][i]
+            newrcv[2,i] = rcv[2][i]
+        newrcv[0,len(rcv[2])] = self.c_row-1
+        newrcv[1,len(rcv[2])] = self.c_col
+        newrcv[2,len(rcv[2])] = 0
+        self.matrix = csr_matrix((newrcv[2], (newrcv[0], newrcv[1])), shape=(self.c_row, self.c_col+1))
+        
+        self.c_col  = self.c_col + 1
+        return True
         pass
 
 
@@ -59,8 +113,27 @@ class CSRSpreadsheet(BaseSpreadsheet):
 
         @return True if operation was successful, or False if not, e.g., rowIndex is invalid.
         """
+        if(rowIndex < 0 ):
+            return False
+        if(rowIndex > self.c_row-1 ):
+            return False
+        rcv = find(self.matrix)
+        newrcv = np.zeros(shape = (3, len(rcv[2]) + 1))
+        for i in range(len(rcv[2])):
+            if(rcv[0][i] >= rowIndex):
+                newrcv[0,i] = rcv[0][i]+1                
+            else:
+                newrcv[0,i] = rcv[0][i]
+            newrcv[1,i] = rcv[1][i]
+            newrcv[2,i] = rcv[2][i]
+            
 
-        # REPLACE WITH APPROPRIATE RETURN VALUE
+        newrcv[0,len(rcv[2])] = self.c_row
+        newrcv[1,len(rcv[2])] = self.c_col-1
+        newrcv[2,len(rcv[2])] = 0
+        self.matrix = csr_matrix((newrcv[2], (newrcv[0], newrcv[1])), shape=(self.c_row+1, self.c_col))
+        
+        self.c_row  = self.c_row + 1
         return True
 
 
@@ -72,7 +145,28 @@ class CSRSpreadsheet(BaseSpreadsheet):
 
         return True if operation was successful, or False if not, e.g., colIndex is invalid.
         """
+        if(colIndex < 0 ):
+            return False
+        if(colIndex > self.c_col-1 ):
+            return False
+        rcv = find(self.matrix)
+        newrcv = np.zeros(shape = (3, len(rcv[2])+1))
+        for i in range(len(rcv[2])):
+            if(rcv[1][i] >= colIndex):
+                newrcv[1,i] = rcv[1][i]+1                
+            else:
+                newrcv[1,i] = rcv[1][i]
+            newrcv[0,i] = rcv[0][i]
+            newrcv[2,i] = rcv[2][i]
+            
 
+        newrcv[0,len(rcv[2])] = self.c_row-1
+        newrcv[1,len(rcv[2])] = self.c_col
+        newrcv[2,len(rcv[2])] = 0
+        self.matrix = csr_matrix((newrcv[2], (newrcv[0], newrcv[1])), shape=(self.c_row, self.c_col+1))
+        
+        self.c_col  = self.c_col+1
+        
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return True
 
@@ -92,7 +186,31 @@ class CSRSpreadsheet(BaseSpreadsheet):
         # TO BE IMPLEMENTED
 
         # REPLACE WITH APPROPRIATE RETURN VALUE
-        return True
+        if(colIndex < 0 or colIndex >= self.c_col):
+            return False
+        if(rowIndex < 0 or rowIndex >= self.c_row):
+            return False
+        rcv = find(self.matrix)
+        newrcv = np.zeros(shape = (3, len(rcv[2])+1))
+        isExist = False
+        for i in range(len(rcv[2])):
+            newrcv[0,i] = rcv[0][i]
+            newrcv[1,i] = rcv[1][i]
+            newrcv[2,i] = rcv[2][i]
+            if(rcv[0][i] == rowIndex and rcv[1][i] == colIndex):
+                rcv[2][i] = value 
+                isExist = True
+        if(isExist):
+            self.matrix = csr_matrix((rcv[2], (rcv[0], rcv[1])), shape=(self.c_row, self.c_col))
+            return True
+        else:
+            newrcv[0,len(rcv[2])] = rowIndex
+            newrcv[1,len(rcv[2])] = colIndex
+            newrcv[2,len(rcv[2])] = value
+            self.matrix = csr_matrix((newrcv[2], (newrcv[0], newrcv[1])), shape=(self.c_row, self.c_col))
+            return True
+        
+        
 
 
     def rowNum(self)->int:
@@ -100,7 +218,8 @@ class CSRSpreadsheet(BaseSpreadsheet):
         @return Number of rows the spreadsheet has.
         """
         # TO BE IMPLEMENTED
-        return 0
+
+        return self.c_row
 
 
     def colNum(self)->int:
@@ -108,7 +227,7 @@ class CSRSpreadsheet(BaseSpreadsheet):
         @return Number of column the spreadsheet has.
         """
         # TO BE IMPLEMENTED
-        return 0
+        return self.c_col
 
 
 
@@ -125,7 +244,13 @@ class CSRSpreadsheet(BaseSpreadsheet):
         # TO BE IMPLEMENTED
 
         # REPLACE WITH APPROPRIATE RETURN VALUE
-        return []
+        result : [(int, int)] = []
+        rcv = find(self.matrix)
+        for i in range(len(rcv[2])):
+            if(rcv[2][i] == value):
+                result.append((rcv[0][i],rcv[1][i]))
+        
+        return result
 
 
 
@@ -134,5 +259,12 @@ class CSRSpreadsheet(BaseSpreadsheet):
         """
         return a list of cells that have values (i.e., all non None cells).
         """
-
-        return []
+        result : [Cell] = []
+        data = self.matrix.toarray()
+        for i in range(self.c_row):
+            for j in range(self.c_col):
+                if(data[i][j] != 0):
+                    result.append(Cell(i,j,data[i][j]))
+        
+        return result
+        
